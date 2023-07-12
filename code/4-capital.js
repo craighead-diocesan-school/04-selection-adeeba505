1926d19056b51e1f616c547e2aa068d136a1808f
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-07-12 04:19:58 +0000

    capital task finished

--- code/4-capital.js
+++ code/4-capital.js
@@ -4,4 +4,16 @@
 
 function capital() {
   // write your code here
+  let Name = prompt("What's you name?")
+  let question = prompt("What's the capital of France " + Name)
+
+  if (question == "paris") {
+    alert("That's right, " + Name)
+  } 
+  else if (question == "Paris") {
+    alert("That's right, " + Name)
+  } 
+  else {
+    alert("Wrong, get smarter " + Name)
+  }
 }
