f504ec7551f2ffdcc45c2cefa10b6f9836bea57a
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-07-12 04:53:57 +0000

    finished the sale calculator

--- code/5-sale-calculator.js
+++ code/5-sale-calculator.js
@@ -4,4 +4,16 @@
 
 function saleCalculator() {
   // write your code here
+  let money = Number(prompt("How much money do you have to spend?"))
+  let item = Number(prompt("How much is the item usually?"))
+  let percentage = Number(prompt("And what is the discount percentage?"))
+
+  let total = item - ((percentage / 100)* item)
+
+  if (total > money) {
+    alert("You don't have enough. You have $" + money + " to spend, but even with " + percentage + "% off, $" + item + " only comes down to " + total)
+  }
+  else if (total < money) {
+    alert ("You can buy this item with the money you have! As it costs $" + total+ 'with the ' +percentage+'% off.')
+  }
 }
