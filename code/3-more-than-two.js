fc811848130ad9c603b2252c0db6a19a66137b0b
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-07-12 04:10:38 +0000

    finished the more than 2 task

--- code/3-more-than-two.js
+++ code/3-more-than-two.js
@@ -4,4 +4,15 @@
 
 function moreThanTwo() {
   // write your code here
+  let digit = Number(prompt('What number do you wanna compare to 2?'))
+
+  if (digit == 2) {
+    alert ('2 is equal to 2')
+  }
+  else if (digit > 2) {
+    alert (digit +' is bigger than 2')
+  }
+  else if (digit < 2) {
+    alert (digit +' is smaller than 2')
+  }
 }
